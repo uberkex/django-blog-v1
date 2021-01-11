02e781a4cfa3e3a334c01e1b2f99b8de7cdd8899
Berke Karataş <uberke.karatas@gmail.com>
2021-01-12 01:09:30 +0300

    first video content done

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "article"
 
 urlpatterns = [
-    path('', views.article_home(), name="article_home"),
+    path('', views.article_home, name="article_home"),
     path('articles/', views.article_list, name="article_list"),
 
 ]
